09aeb0a77b57d7d460a07c4e936bce94da60cf43
Hello-alt 242 <jbv77777@gmail.com>
2020-07-21 11:21:09 -0400

    16

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using API.Dtos;
 using AutoMapper;
+using Core.Entities;
 using Core.Entities.Identity;
 using Core.Entitites;
 
@@ -14,6 +15,8 @@ namespace API.Helpers
                 .ForMember(d=>d.ProductType, o=>o.MapFrom(s=>s.ProductType.Name))
                 .ForMember (d=>d.PictureUrl, o=>o.MapFrom<ProductUrlResolver>());
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto,CustomerBasket>();
+            CreateMap<BasketItemDto,BasketItem>();
         }
     }
 }
